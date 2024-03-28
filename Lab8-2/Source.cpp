cb989d1fbf11092064ac6019fb8b1b7e75d2aacd
c-perl <c-perl@onu.edu>
2024-03-28 09:26:05 -0400

    Word counter

---
+++ Lab8-2/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countWords(const string& str) {
+    int count = 0;
+    bool inWord = false;
+    for (char ch : str) {
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
+            if (inWord) {
+                count++;
+                inWord = false;
+            }
+        }
+        else {
+            if (!inWord) {
+                inWord = true;
+            }
+        }
+    }
+    
+    if (inWord) {
+        count++;
+    }
+    return count;
+}
+
+int main(void) {
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+
+		int stringLength = countWords(input);
+		cout <<"Word count: "<< stringLength << endl;
+
+
+	}
+	return 0;
+}
